0223cd95a81e88604163f844802ef34d9882cb5d
Bruno Máximo <Bmaximo93@gmail.com>
2026-03-21 11:16:52 -0300

    :sparkles: feat: adicionar model Recipe

--- copytaste/models.py
+++ copytaste/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Recipe(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes') # relação de 1:N com User
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    ingredients = models.TextField()
+    steps = models.TextField()
+    duration_minutes = models.IntegerField(null=True, blank=True)
+    source_url = models.URLField(blank=True, max_length=300) # TODO: ver se 300 é suficiente pra link do youtube
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
